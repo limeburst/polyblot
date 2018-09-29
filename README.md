ff03d64c4b52f5bd2b2ed0586d23651e33930051
Jihyeok Seo <ji@hyeok.org>
2018-09-29 18:24:17 +0900

    Add live reloading and contribution docs

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ verbose-engine
 ==============
 
 github gave me this name.
+
+
+Contributing
+------------
+
+```bash
+$ cargo install systemfd cargo-watch
+$ systemfd --no-pid -s http::3000 -- cargo watch -x run 
+$ open http://localhost:3000
+```
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 extern crate actix_web;
+extern crate listenfd;
 extern crate fluent_locale;
 extern crate accept_language;
 
+use listenfd::ListenFd;
 use actix_web::{server, App, HttpRequest, Responder};
 use fluent_locale::Locale;
 
@@ -21,11 +23,17 @@ fn print_chosen_language(req: &HttpRequest) -> impl Responder {
 }
 
 fn main() {
-    server::new(|| {
+    let mut listenfd = ListenFd::from_env();
+    let mut server = server::new(|| {
         App::new()
             .resource("/", |r| r.f(print_chosen_language))
-    })
-    .bind("127.0.0.1:8000")
-    .expect("Can not bind to port 8000")
-    .run();
+    });
+
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l)
+    } else {
+        server.bind("127.0.0.1:3000").unwrap()
+    };
+
+    server.run();
 }
