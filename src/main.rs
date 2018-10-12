baf8ba758def53b1131f31d904407ec93ef67f71
Jihyeok Seo <ji@hyeok.org>
2018-10-12 14:13:41 +0900

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
-extern crate actix_web;
-extern crate listenfd;
-extern crate fluent_locale;
 extern crate accept_language;
+extern crate actix_web;
+extern crate fluent_locale;
+extern crate listenfd;
 
-use listenfd::ListenFd;
 use actix_web::{server, App, HttpRequest, Responder};
 use fluent_locale::Locale;
+use listenfd::ListenFd;
 
-fn choose_language(req: &HttpRequest) -> String
-{
-    let accept_language = req.headers().get("accept-language").unwrap().to_str().unwrap();
+fn choose_language(req: &HttpRequest) -> String {
+    let accept_language = req
+        .headers()
+        .get("accept-language")
+        .unwrap()
+        .to_str()
+        .unwrap();
     let parsed = accept_language::parse(accept_language);
-    let locale = Locale::from(parsed[0].clone());  // FIXME
+    let locale = Locale::from(parsed[0].clone()); // FIXME
 
     locale.get_language().to_string()
 }
@@ -24,10 +28,7 @@ fn print_chosen_language(req: &HttpRequest) -> impl Responder {
 
 fn main() {
     let mut listenfd = ListenFd::from_env();
-    let mut server = server::new(|| {
-        App::new()
-            .resource("/", |r| r.f(print_chosen_language))
-    });
+    let mut server = server::new(|| App::new().resource("/", |r| r.f(print_chosen_language)));
 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen(l)
